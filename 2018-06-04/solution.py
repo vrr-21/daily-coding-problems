d50f0cbe9f4b35d85545645fe3d4d180ce82c0d6
vrr-21 <varun21exps@gmail.com>
2018-07-05 08:55:09 +0530

    Part 1 of solution for 4th July 2018

---
+++ 2018-06-04/solution.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def serialize(node):
+    serialized_string = ""
+    if node == None:
+        serialized_string = "/"
+    else:
+        serialized_string = serialized_string + str(node.val) + ","
+        serialized_string = serialized_string + serialize(node.left) + ","
+        serialized_string = serialized_string + serialize(node.right)
+    return serialized_string
+    
+if __name__ == '__main__':
+    node = Node(1, Node(3, Node(4)), Node(2))
+    print(serialize(node))
