44d05c75d62ee30ae58d38aa132aa71c96e36358
vrr-21 <varun21exps@gmail.com>
2018-07-14 02:26:25 +0530

    Solution for 13th July problem.

--- 2018-07-13/solution.py
+++ 2018-07-13/solution.py
@@ -5,6 +5,20 @@ def solver_wo_follow(n):
         memory.append(memory[-1] + memory[-2])
     return memory[-1] + memory[-2]
 
+def solver_w_follow(n,stepList):
+    # In this function, stepList is the list of number of steps which can be taken at one point.
+    if n == 0:
+        return 1
+    memory = [1]
+    for i in range(1,n+1):
+        totalSteps = 0
+        for steps in stepList:
+            if i >= steps:
+                totalSteps = totalSteps + memory[i - steps]
+        memory.append(totalSteps)
+    return memory[n]
+
 if __name__ == '__main__':
     n = int(input("Enter N:"))
     print(solver_wo_follow(n))
+    print(solver_w_follow(n,[1,2]))
